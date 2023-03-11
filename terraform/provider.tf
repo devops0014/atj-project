6a53f1c6d928b2185b5be5db6a8bac5776157012
devops0014 <106308110+devops0014@users.noreply.github.com>
2023-03-11 18:26:04 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = "AKIARSPNELGYDOMKNLFP"
-secret_key = "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"
+access_key = "AKIARSPNELGYMRNOE5DS"
+secret_key = "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"
 }
